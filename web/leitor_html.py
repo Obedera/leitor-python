01b537c1fbb1c464bbe09d7a9ba5b8fde4660a1e
Obede <obede.silva7@gmail.com>
2019-07-31 12:18:00 -0300

    Mais funções no leitor html

--- web/leitor_html.py
+++ web/leitor_html.py
@@ -1,14 +1,58 @@
 def analizar_html(html):
     texto_html = html.read()
     linha_html = texto_html.splitlines()
-    contador_linha_html = 0
+    Erros = ''
 
-    while contador_linha_html<len(linha_html):
-        palavras = linha_html[contador_linha_html].split()
-        for j in palavras:
-            if j == '<bodyy>':
-                print(f'Tem erro na linha {contador_linha_html+1} o "{j}" está escrito errado')
-                
-            if j == '<scripts':
-                print(f'Tem erro na linha {contador_linha_html+1} o "{j}" está escrito errado')    
-        contador_linha_html += 1
+    def quebrar_por_palavra(linha):
+        contador_linha_html = 0
+        palavras = []
+        while contador_linha_html<len(linha):
+            palavra = linha[contador_linha_html].split()
+            for i in palavra:
+                palavras.append(i)  
+            contador_linha_html += 1
+        return palavras
+
+    lista_palavras = quebrar_por_palavra(linha_html)
+
+    def quebrar_por_letra(lista_palavras):
+        contador_letra_html = 0
+        letras = []
+        while contador_letra_html<len(lista_palavras):
+            letra = lista_palavras[contador_letra_html]
+            for i in letra:
+                letras.append(i)
+            contador_letra_html += 1
+        return letras
+    
+    lista_letras = quebrar_por_letra(lista_palavras)
+
+    if lista_letras.count('<') > lista_letras.count('>'):
+        Erros += 'Você esqueceu de colocar ">"\n'
+    if lista_letras.count('<') < lista_letras.count('>'):
+        Erros += 'Você esqueceu de colocar "<"\n'
+
+    def checar_erros_palavra(lista_palavras):
+        contador = 0
+        erros = ''
+        while contador<len(lista_palavras):
+            if lista_palavras[contador] == '<bodyy>' or lista_palavras[contador] == '<bodyy':
+                erros += f'Tem erro na linha {contador+1} o "{lista_palavras[contador]}" está escrito errado\n'
+                   
+            if lista_palavras[contador] == '<scripts':
+                erros += f'Tem erro na linha {contador+1} o "{lista_palavras[contador]}" está escrito errado\n'
+
+            contador += 1
+
+        return erros
+
+
+    Erros += checar_erros_palavra(lista_palavras)
+
+    return Erros
+    
+    
+    
+
+
+
--- web/principal.py
+++ web/principal.py
@@ -4,9 +4,9 @@ from leitor_js import analizar_js
 
 
 html = open('index.html')
-print('No HTML')
-analizar_html(html)
+print('---------No HTML---------')
+print(analizar_html(html))
 
-print('\nNo JS')
+print('\n---------No JS---------')
 js = open('script.js')
 analizar_js(js)
