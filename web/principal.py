92f7cd5909baebd1e7b194b20ec548c47815a09b
Obede <obede.silva7@gmail.com>
2019-08-01 16:49:51 -0300

    ler css

--- web/principal.py
+++ web/principal.py
@@ -1,5 +1,6 @@
 from leitor_html import analizar_html
 from leitor_js import analizar_js
+from leitor_css import analizar_css
 
 
 
@@ -10,3 +11,7 @@ print(analizar_html(html))
 print('\n---------No JS---------')
 js = open('script.js')
 print(analizar_js(js))
+
+print('\n---------No CSS---------')
+css = open('style.css')
+print(analizar_css(css))
