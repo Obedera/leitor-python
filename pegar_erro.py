379bee5828fa53dcfe08ab5ea9f2055ad52b2e24
Obede <obede.silva7@gmail.com>
2019-08-01 17:29:01 -0300

    Bug na tradução

--- pegar_erro.py
+++ pegar_erro.py
@@ -1,11 +1,23 @@
 from googletrans import Translator
 translator = Translator()
 try:
-    print(ws)
+    nome = 'Obede'
+    if nome == 'Obede':
+        print(nome)
+        print(sobrenome)
+
 except Exception as erro:
     arq = open('texto.txt', 'w')
     texto = str(erro)
-    texto_traduzido = translator.translate(texto,dest='pt').text
-    arq.write(texto_traduzido)
+    contador = 0
+    posicao = 0
+    while contador<len(texto):
+        if texto[contador] == "'":
+            posicao = contador
+        contador += 1
+
+    texto_traduzir = texto[posicao+1:]
+    texto_traduzido = translator.translate(texto_traduzir,dest='pt').text
+    arq.write(f'Erro aqui {texto[5:posicao+1]} {texto_traduzido}')
     arq.close()
-    print(f'erro aqui {texto_traduzido}')
+    print(f'Erro aqui {texto[5:posicao+1]} {texto_traduzido}')
